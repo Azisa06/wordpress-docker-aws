7df9caad99c6d4eef4dfb88fec08643bcc459f29
Isabella <isabella.c.thomazini@gmail.com>
2025-05-02 09:53:07 -0300

    Melhorias

--- user_data.sh
+++ user_data.sh
@@ -1,33 +1,30 @@
 #!/bin/bash
-# Atualiza pacotes
-sudo apt-get update -y
-sudo apt-get upgrade -y
+#sudo apt-get update -y
+#sudo apt-get upgrade -y
+#sudo apt-get install -y docker.io
+#sudo systemctl enable docker
+#sudo systemctl start docker
+#sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#sudo apt-get install -y git
+#cd /home/ubuntu
+#git clone https://github.com/Azisa06/wordpress-docker-aws.git wordpress-app
+#cd wordpress-app
+#sudo docker-compose up -d
 
-# Instala Docker
-sudo apt-get install -y docker.io
-
-# Habilita e inicia o Docker
-sudo systemctl enable docker
-sudo systemctl start docker
-
-# Instala Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-# Instala git
-sudo apt-get install -y git
-
-# Clona seu repositório (🔁 Substitua pela URL do seu Git)
-cd /home/ubuntu
-git clone https://github.com/seu-usuario/seu-repositorio.git wordpress-app
+#!/bin/bash
+apt update -y
+apt install -y docker.io nfs-common
+systemctl start docker
+systemctl enable docker
+mkdir -p /usr/local/lib/docker/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose
+chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+EFS_ID=fs-04aec50e2e2bdfd4e.efs.us-east-1.amazonaws.com                
+REGIAO=us-east-1
+mkdir -p /mnt/efs
+mount -t nfs4 -o nfsvers=4.1 ${EFS_ID}.efs.${REGIAO}.amazonaws.com:/ /mnt/efs
+chown -R 33:33 /mnt/efs
+git clone https://github.com/Azisa06/wordpress-docker-aws.git wordpress-app
 cd wordpress-app
-
-# (Opcional) Monta o EFS - substitua o DNS pelo seu
-# sudo apt-get install -y nfs-common
-# sudo mkdir -p /mnt/efs
-# sudo mount -t nfs4 -o nfsvers=4.1 fs-XXXXXX.efs.REGION.amazonaws.com:/ /mnt/efs
-# sudo chown -R 1000:1000 /mnt/efs
-# (Use volumes no docker-compose para apontar /var/www/html/wp-content para /mnt/efs)
-
-# Sobe os containers
-sudo docker-compose up -d
+docker compose up -d
