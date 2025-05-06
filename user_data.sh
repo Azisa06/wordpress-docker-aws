44d23828ffbc14f41a4437f6704cfc35cbf84dc2
Isabella <isabella.c.thomazini@gmail.com>
2025-05-06 09:08:57 -0300

    Adicionando criação automática de BD no user_data

--- user_data.sh
+++ user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y docker.io
+sudo apt-get install -y docker.io mysql-client git
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -11,3 +11,5 @@ cd /home/ubuntu
 git clone https://github.com/Azisa06/wordpress-docker-aws.git wordpress-app
 cd wordpress-app
 sudo docker-compose up -d
+
+mysql -h database-wordpress.cgrqa20q0xnf.us-east-1.rds.amazonaws.com -u admin -pminhasenha123 -e "CREATE DATABASE IF NOT EXISTS wordpress_db;"
